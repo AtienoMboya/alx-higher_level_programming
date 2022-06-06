adc3c8b2f851df053a7b8201f521ab1cdc6d1527
AtienoMboya <sharonmboya894@gmail.com>
2022-06-06 17:08:05 +0300

    multiple returns

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def multiple_returns(sentence):
+    slist=[]
+    slist[:0] = sentence
+    if len(slist) == 0:
+        slist[0] = None
+    tup = (len(slist), slist[0])
+    return tup
