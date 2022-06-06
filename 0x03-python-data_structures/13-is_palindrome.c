042b559c56d781cfa83ef0c246cacb74ed20f90c
AtienoMboya <68197008+AtienoMboya@users.noreply.github.com>
2022-06-06 19:36:46 +0300

    Update 13-is_palindrome.c

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -4,10 +4,10 @@ listint_t *reverse_listint(listint_t **head);
 int is_palindrome(listint_t **head);
 
 /**
- * reverse_listint - Reverses a singly-linked listint_t list
- * @head: A pointer to the startign node of the list to reverse
+ * reverse_listint - Reverses a singly-linked listint_t list.
+ * @head: A pointer to the starting node of the list to reverse.
  *
- * Return: A pointer to the head of the reversed list
+ * Return: A pointer to the head of the reversed list.
  */
 listint_t *reverse_listint(listint_t **head)
 {
@@ -24,11 +24,13 @@ listint_t *reverse_listint(listint_t **head)
 	*head = prev;
 	return (*head);
 }
+
 /**
- * is_palindrome - checks if a singly linked list is a palindrome
- * @head: a pointer to the head of the linked list
+ * is_palindrome - Checks if a singly linked list is a palindrome.
+ * @head: A pointer to the head of the linked list.
  *
- * Return: If the linked list is not a palindrome - 0
+ * Return: If the linked list is not a palindrome - 0.
+ *         If the linked list is a palindrome - 1.
  */
 int is_palindrome(listint_t **head)
 {
@@ -46,7 +48,7 @@ int is_palindrome(listint_t **head)
 	}
 
 	tmp = *head;
-	for (i = 0 ; i < (size / 2) - 1 ; i++)
+	for (i = 0; i < (size / 2) - 1; i++)
 		tmp = tmp->next;
 
 	if ((size % 2) == 0 && tmp->n != tmp->next->n)
