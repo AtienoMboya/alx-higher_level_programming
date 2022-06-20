8022ce180a436f88ba19395fd0ae7997bb07e9e0
AtienoMboya <68197008+AtienoMboya@users.noreply.github.com>
2022-06-21 01:47:20 +0300

    fixed pycodestyle

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -3,14 +3,13 @@ import sys
 
 
 def safe_function(fct, *args):
-    """executes a function safely
+    """Executes a function safely.
     Args:
-        fct: pointer to a function
-        args: Arguments for fct
-
+        fct: The function to execute.
+        args: Arguments for fct.
     Returns:
-        if an error occurs - None
-        Otherwise - the result of the call to fct
+        If an error occurs - None.
+        Otherwise - the result of the call to fct.
     """
     try:
         result = fct(*args)
