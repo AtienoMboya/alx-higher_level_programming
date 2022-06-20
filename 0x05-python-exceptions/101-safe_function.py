ff19d69c6a34b08f977c9b9e25e9d8154efac5e0
AtienoMboya <sharonmboya894@gmail.com>
2022-06-20 22:15:57 +0300

    safe function

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+import sys
+def safe_function(fct, *args):
+    """executes a function safely
+    Args:
+        fct: pointer to a function
+        args: Arguments for fct
+
+    Returns:
+        if an error occurs - None
+        Otherwise - the result of the call to fct
+    """
+    try:
+        result = fct(*args)
+        return (result)
+    except:
+        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
+        return (None)
