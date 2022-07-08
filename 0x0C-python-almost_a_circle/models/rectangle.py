ea98833a1896f706fd9be3c190987a876aa50f44
AtienoMboya <sharonmboya894@gmail.com>
2022-07-08 23:07:52 +0300

    test and started rectangle

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Rectangle class inherits from Base class."""
+
+
+class Rectangle(Base):
+    """class Rectangle that inherits from Base.
+
+    Attributes:
+        __width: private instance attribute
+        __height: pribate instance attribute
+        __x: private instance attribute
+        __y: private instance attribute
+    """
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Initializes the class.
+
+        Args:
+            width (int): the width of the rectangle
+            height (int): the height of the rectangle
+            x (int)
+            y (int)
+        """
+
+
