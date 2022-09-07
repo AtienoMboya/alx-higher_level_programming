8d770047e6b7be204b85043696adc4ebcfa62296
AtienoMboya <sharonmboya894@gmail.com>
2022-09-07 05:18:45 +0300

    fixed typo

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-exports.exrever = function (list) {
+exports.esrever = function (list) {
   return list.reduceRight(function (array, current) {
     array.push(current);
     return array;
