d52d26af8513c253fa517a9bf69e1e4cd3330915
AtienoMboya <sharonmboya894@gmail.com>
2022-09-11 21:41:05 +0300

    fix typo

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 """
 Lists all State objects from the database hbtn_0e_6_usa.
+Usage: ./7-model_state_fetch_all.py <mysql username> /
+                                    <mysql password> /
+                                    <database name>
 """
-
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model.state import State
+from model_state import State
 
 if __name__ == "__main__":
     engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
