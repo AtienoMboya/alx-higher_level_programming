06f9346a29a6cbdb10316657a6aa4efe80e8e84f
AtienoMboya <sharonmboya894@gmail.com>
2022-09-22 21:35:01 +0300

    fix

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -2,8 +2,8 @@
 """finding peak module"""
 
 
-def finding_peak(list_of_integers):
-    """Returns a peak in a list of unsorted integers"""
+def find_peak(list_of_integers):
+    """Return a peak in a list of unsorted integers."""
     if list_of_integers == []:
         return None
 
